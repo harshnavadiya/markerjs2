9308f3327ed54d8af2f010f593dcbf11e678864d
Mili Virani <Navadiyaha@gmail.com>
2024-07-03 18:18:31 +0530

    WIP: Pen Marker

--- src/markers/pan/PenToolState.ts
+++ src/markers/pan/PenToolState.ts
@@ -1,24 +1,9 @@
 import { IPoint } from '../../core/IPoint';
 import { LinearMarkerBaseState } from '../LinearMarkerBaseState';
 
-/**
- * Represents state of a {@link CurveMarker}.
- */
 export interface PenToolState extends LinearMarkerBaseState {
-  /**
-   * Line color.
-   */
-  strokeColor: string,
-  /**
-   * Line width.
-   */
-  strokeWidth: number,
-  /**
-   * Line dash array.
-   */
-  strokeDasharray: string,
-
-  curveX?: number,
-  curveY?: number,
-  points: any
+  strokeColor: string;
+  strokeWidth: number;
+  // strokeDasharray: string;
+  points: IPoint[];
 }
